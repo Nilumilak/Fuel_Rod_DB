ddd220d2abefb642cc0c843db471e275d7f5a657
Renat Kalimulin <pianoparrot93@gmail.com>
2022-10-22 17:57:40 +0300

    Initial commit

---
+++ Fuel_Rod_DB/dry_storage/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DryStorageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dry_storage'
---
+++ Fuel_Rod_DB/fresh_inventory/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FreshInventoryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'fresh_inventory'
