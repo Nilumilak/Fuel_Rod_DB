1cf7b052f08c0b3b15db7d890a6d8d656caa50f4
Renat Kalimulin <pianoparrot93@gmail.com>
2022-10-23 23:49:42 +0300

    Added models: RodPieceNote, AnalysisTechnique, SampleState, RodPiece Models added to admin panel: RodPieceNote, AnalysisTechnique, SampleState, RodPiece

--- Fuel_Rod_DB/rod_pieces/admin.py
+++ Fuel_Rod_DB/rod_pieces/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from rod_pieces.models import RodPieceNote, AnalysisTechnique, SampleState, RodPiece
+
+
+@admin.register(RodPieceNote)
+class RodPieceNoteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AnalysisTechnique)
+class AnalysisTechniqueAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SampleState)
+class SampleStateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RodPiece)
+class RodPieceAdmin(admin.ModelAdmin):
+    list_display = ['rod_id', 'analysis_technique', 'sample_state', 'created_by', 'updated_by']
+    fields = ['material', 'analysis_technique', 'sample_state', 'created_by', 'updated_by']
+    list_editable = ['sample_state', 'created_by', 'updated_by']
--- Fuel_Rod_DB/rod_pieces/models.py
+++ Fuel_Rod_DB/rod_pieces/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class RodPieceNote(models.Model):
+    rod = models.ForeignKey('RodPiece', on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+
+class AnalysisTechnique(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class SampleState(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class RodPiece(models.Model):
+    rod_id = models.CharField(max_length=100, blank=True, null=True)
+    material = models.CharField(max_length=100)
+    number = models.IntegerField()
+    analysis_technique = models.ForeignKey(AnalysisTechnique, on_delete=models.CASCADE)
+    sample_state = models.ForeignKey(SampleState, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='piece_user_created')
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='piece_user_updated')
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if not self.rod_id:
+            self.number = RodPiece.objects.filter(material=self.material, analysis_technique=self.analysis_technique).count() + 1
+        self.rod_id = f'{self.material}-{self.analysis_technique}{self.number:02}{"-R" if self.sample_state.name == "Ready" else ""}'
+        super().save()
+
+    def __str__(self):
+        return self.rod_id
