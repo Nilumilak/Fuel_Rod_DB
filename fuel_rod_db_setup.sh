0197d0efb0fa7a8914181ccefab9891d61b450f7
Renat Kalimulin <pianoparrot93@gmail.com>
2022-10-30 00:29:42 +0300

    Added bash scripts for installing and launching

---
+++ fuel_rod_db_setup.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+pip install virtualenv
+winpty python -m venv venv
+source venv/Scripts/activate
+pip install -r requirements.txt
+python Fuel_Rod_DB/manage.py makemigrations fresh_inventory
+python Fuel_Rod_DB/manage.py makemigrations temperature_excursions_exp
+python Fuel_Rod_DB/manage.py makemigrations dry_storage_exp
+python Fuel_Rod_DB/manage.py makemigrations temperature_excursions
+python Fuel_Rod_DB/manage.py makemigrations dry_storage
+python Fuel_Rod_DB/manage.py makemigrations rod_pieces
+python Fuel_Rod_DB/manage.py migrate
+python Fuel_Rod_DB/manage.py loaddata data.json
+winpty python Fuel_Rod_DB/manage.py createsuperuser
